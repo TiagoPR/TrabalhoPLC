d3da7ed5187ce1b293ad2a19d5eaa39fa75d62eb
brenin <marraob@gmail.com>
2022-10-26 10:09:10 +0100

    clones

--- Ex1/frequencia_ano.py
+++ Ex1/frequencia_ano.py
@@ -1,16 +1,19 @@
 import re
 
 f = open("processos.txt", "r")
+ant = ""
 dic = {}
 ano = ""
 for linha in f:
-    y = re.match(r'([1-9][0-9]*)::([0-9]{4})', linha)
-    if y:
-        ano = y.group(2)
-        if ano not in dic:
-            dic[ano] = 1
-        else:
-            dic[ano] += 1
+    if ant != linha:
+        y = re.match(r'([1-9][0-9]*)::([0-9]{4})', linha)
+        if y:
+            ano = y.group(2)
+            if ano not in dic:
+                dic[ano] = 1
+            else:
+                dic[ano] += 1
+    ant = linha
 
 frase = input("Qual o ano? ")
 while frase != "":
