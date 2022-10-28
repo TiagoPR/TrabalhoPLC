c4820326f00aa3f0f9106f5782ae81d8a233485f
brenin <marraob@gmail.com>
2022-10-28 10:44:51 +0100

    mais avancado

--- csv_to_json.py
+++ csv_to_json.py
@@ -6,14 +6,27 @@ primeira = next(f)
 
 atributos = re.split(',',primeira)
 atributos[-1] = atributos[-1][:-1]
+a = 0
+for atributo in atributos:
+    if atributo == '':
+        break
+    a += 1
+
+print(atributos[:a])
 tamanho = len(atributos)
 dic = {}
+
 for linha in f:
     valores = re.split(',',linha)
-    for j in range(tamanho):
+    for j in range(a-2):
+        '''
         if tamanho-1 == j:
             dic[atributos[j]] = valores[j][:-1]
         else:
-            dic[atributos[j]] = valores[j]
+        '''
+        dic[atributos[j]] = valores[j]
+    dic[atributos[a-1]] = []
+    for j in range(a-1,tamanho):
+        dic[atributos[a-1]].append(valores[j])
     list.append(dic)
 print(list)
